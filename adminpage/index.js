b52e051593d976926f56d30aca2c3a0b8a79714a
jhdev3 <jacobheden@gmail.com>
2021-12-01 12:55:16 +0100

    Update
    Index.js

--- adminpage/index.js
+++ adminpage/index.js
@@ -1,1 +1,28 @@
 //ServerScript för manaBar
+//Använder express
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+
+/* Routing refers to determining how an application responds to a client request to a particular endpoint, 
+which is a URI (or path) and a specific HTTP request method (GET, POST, and so on).
+Res = respond, req är request :)
+
+*/
+
+app.get('/', (req, res) => {
+
+
+  //Res är 
+  res.send('<h1>HEj Hej Express</h1>'); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
+
+});
+
+// Internet-protokollen TCP och UDP använder portar numrerade 0–65 535
+server.listen(4000, () => {  
+  console.log('listening on *:4000');
+});
+console.log("Hejsan");
