4ad89db13c9a66f91ede40f8017d2f25abf1f493
Uroise <simon.hultqvist@gmail.com>
2021-12-03 16:28:18 +0100

    d

--- script/bokadator.js
+++ script/bokadator.js
@@ -9,12 +9,11 @@ function validateForm() {
     $("#bookPcText").hide();
     document.getElementById("BookPcPlace").innerHTML = "Tack för din bokning";
     }
-    
-}
-let today = new Date();
+    let today = new Date();
         let dd = String(today.getDate()).padStart(2, '0');
         let mm = String(today.getMonth() + 1).padStart(2, '0');
         let yyyy = today.getFullYear();
 
         today = yyyy + '-' + mm + '-' + dd;
         $('#datePc').attr('min',today);
+}
