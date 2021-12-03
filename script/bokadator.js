394050526d53b3dc917799c6dcaaecdc136c3dee
Uroise <simon.hultqvist@gmail.com>
2021-12-03 16:18:58 +0100

    Fixed
    Fixed mail and date

--- script/bokadator.js
+++ script/bokadator.js
@@ -1,8 +1,15 @@
 function validateForm() {
+    let format = /[ `!#$%^&*()_+\-=\[\]{};':"\\|,<>\/?~]/;
+    if(document.getElementById("emailPc").value.match(format)){
+        alert("Email cannot contain one of the entered characters")
+    }
+    else{
     $("#bookPc").hide();
     $("#pcBookedButton").hide();
     $("#bookPcText").hide();
     document.getElementById("BookPcPlace").innerHTML = "Tack för din bokning";
+    }
+    
 }
 let today = new Date();
         let dd = String(today.getDate()).padStart(2, '0');
