24d396965a0e100bf47bd322f8f8cb00d630f53b
Uroise <simon.hultqvist@gmail.com>
2021-12-03 10:52:51 +0100

    Uppdates
    Cursor pointer när man hovrar på button
    If+statement så input inte empty

--- script/bokadator.js
+++ script/bokadator.js
@@ -1,1 +1,21 @@
-console.log("bokaDatorJs");
+function validateForm()
+{
+    if(document.getElementById("namePc").value.length == 0){
+        alert("Name cannot be empty")
+    }
+    if(document.getElementById("emailPc").value.length == 0){
+        alert("Email cannot be empty")
+    }
+    if(document.getElementById("numberPc").value.length == 0){
+        alert("Number cannot be empty")
+    }
+    if(document.getElementById("datePc").value.length == 0){
+        alert("Date cannot be empty")
+    }
+    if(document.getElementById("timePc").value.length == 0){
+        alert("Time cannot be empty")
+    }
+    else{
+    $("#bookPc").hide();
+    }
+}
