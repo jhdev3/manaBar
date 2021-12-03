01274e1e2f17e561d3a911f3637bf2f87f26bd00
Uroise <simon.hultqvist@gmail.com>
2021-12-03 12:48:46 +0100

    Change
    Tog bort onödig kod
    Lade till kod som kontrollerar att man inte kan välja datum tidigare än dagens datum

--- script/bokadator.js
+++ script/bokadator.js
@@ -1,21 +1,13 @@
-function validateForm()
-{
-    if(document.getElementById("namePc").value.length == 0){
-        alert("Name cannot be empty")
-    }
-    if(document.getElementById("emailPc").value.length == 0){
-        alert("Email cannot be empty")
-    }
-    if(document.getElementById("numberPc").value.length == 0){
-        alert("Number cannot be empty")
-    }
-    if(document.getElementById("datePc").value.length == 0){
-        alert("Date cannot be empty")
-    }
-    if(document.getElementById("timePc").value.length == 0){
-        alert("Time cannot be empty")
-    }
-    else{
+function validateForm() {
     $("#bookPc").hide();
-    }
+    $("#pcBookedButton").hide();
+    $("#bookPcText").hide();
+    document.getElementById("BookPcPlace").innerHTML = "Tack för din bokning";
 }
+let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+
+        today = yyyy + '-' + mm + '-' + dd;
+        $('#datePc').attr('min',today);
