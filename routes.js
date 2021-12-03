3df5cd435209d4cdd6266b87870e676faa92957a
jhdev3 <jacobheden@gmail.com>
2021-12-03 10:26:26 +0100

    Routes
    Updaterade
    Enkel check i if satsen
    
    Checken på servern bör göras mer grundlig

--- routes.js
+++ routes.js
@@ -2,20 +2,13 @@ let express = require("express");
 
 let router = express.Router(); //Alla routes lagras i router
 
-
-
-router.get('/', (req, res) => {
-
-
-    //Res är 
-    res.sendFile(__dirname +  "/index.html"); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
-  
+router.get("/", (req, res) => {
+  //Res är
+  res.sendFile(__dirname + "/index.html"); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
 });
 
-router.get('/admin', (req, res) => {
-
-    res.sendFile(__dirname +  "/adminpage/admin.html"); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
-
+router.get("/admin", (req, res) => {
+  res.sendFile(__dirname + "/adminpage/admin.html"); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
 });
 
 //app.use(express.urlencoded());  samma som express.json gissar jag för body och post
@@ -23,54 +16,63 @@ router.get('/admin', (req, res) => {
 
 */
 //post req.body."name" fungerar i post inte i get xD
-router.get('/admin/login', (req, res) => {
-    const adminPassword = "Password123"; //adminPassword Kolla med namn + regexp om man känner för det. borde crypteras.
-    const adminUser ="Admin";
-    let userAray = {name: adminUser, password: adminPassword};
-    let bokaBord = ['item1', 'item2', 'osv'];
-    let bokaDator = ['boakdatorobjek1', 'boakdatorobjek1', 'osv'];
-    
-    console.log(req.query.userAdmin); 
-    console.log(req.query.password);
+router.get("/admin/login", (req, res) => {
+  const adminPassword = "Password123"; //adminPassword Kolla med namn + regexp om man känner för det. borde crypteras.
+  const adminUser = "Admin";
+  let userAray = { name: adminUser, password: adminPassword };
+  let bokaBord = ["item1", "item2", "osv"];
+  let bokaDator = ["boakdatorobjek1", "boakdatorobjek1", "osv"];
 
-    if(req.query.userAdmin == adminUser && req.query.password == adminPassword){
-        //res.send(bokaBord + ' <---Bokabord   Bokadator---> ' + bokaDator);
-        res.send("loginToken: 12312312378126asd1237123hajsdkj81238");
-        console.log("Inlogg succesfull");
-        console.log(bokaBord);
-        console.log(bokaDator);
+  console.log(req.query.userAdmin);
+  console.log(req.query.password);
 
-    }
-    else{
-        console.log("Felaktigt lössenord eller användare: " + req.query.userAdmin  + " " + "  " + req.query.password);
-        res.status(403); //Kan kolla status när den försöker läsa får inte komma dit typ error 403 frobbiden acces //Det där funkar :)
-    }
+  if (req.query.userAdmin == adminUser && req.query.password == adminPassword) {
+    //res.send(bokaBord + ' <---Bokabord   Bokadator---> ' + bokaDator);
+    res.send("loginToken: 12312312378126asd1237123hajsdkj81238");
+    console.log("Inlogg succesfull");
+    console.log(bokaBord);
+    console.log(bokaDator);
+  } else {
+    console.log(
+      "Felaktigt lössenord eller användare: " +
+        req.query.userAdmin +
+        " " +
+        "  " +
+        req.query.password
+    );
+    res.status(403); //Kan kolla status när den försöker läsa får inte komma dit typ error 403 frobbiden acces //Det där funkar :)
+  }
 });
 
-router.get('/booking/array', (req, res) => {
-    if(req.query.token == "12312312378126asd1237123hajsdkj81238"){ //Kollar behöverighet säker nät
-    res.send('lyckad post'); //Inte svenska bokstäver med res.end  men med res.send kmr köra express resten av kursen
-    }
-  });
+router.get("/booking/", (req, res) => {
+  if (req.query.token == "12312312378126asd1237123hajsdkj81238") {
+    //Kollar behöverighet säker nät
+    res.send("lyckad post"); //Inte svenska bokstäver med res.end  men med res.send kmr köra express resten av kursen
+  }
+});
 
 //app.use(express.urlencoded());  samma som express.json gissar jag för body och post
 
 //post req.body."name" fungerar i post inte i get xD
-router.post('/booking/send/bord', (req, res) => {
-    res.send('lyckad post'); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
-  
-  });
+router.post("/api/bokabord", (req, res) => {
+  if (req.body[0] != null) {
+    res.status(201).send("lyckad bookning");
+  } else {
+    res.status(4006).send("vad försöker du göra?");
+  }
+});
 
-router.post('/booking/send/dator', (req, res) => {
-    res.send('lyckad post'); //Inte svenska bokstäver med res.end men med res.send kmr köra express resten av kursen
-  
-  });
-
-
+router.post("/api/bokadator", (req, res) => {
+  if (req.body[0] != null) {
+    res.status(201).send("lyckad bookning");
+  } else {
+    res.status(4006).send("vad försöker du göra?");
+  }
+});
 
 module.exports = router;
 
-//    
+//
 //    <script src="javascript/gbook.js"></script>
 
 /* */
