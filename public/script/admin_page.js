69b3499ab4d5300ca1077a4ba55a22a2eb784406
Jacob Hedén <91886824+jhdev3@users.noreply.github.com>
2021-12-09 16:12:47 +0100

    Funktion för att loggain
    Hämta data behöver bara läsa och skriva från fil :)

--- public/script/admin_page.js
+++ public/script/admin_page.js
@@ -1,16 +1,53 @@
+let adminToken = "";
+
+/* Admin loggin*/
 $("#adminLoggin").submit((e) => {
-  console.log("test");
   e.preventDefault();
-  console.log($("#adminLoggin").serialize());
+  //console.log($("#adminLoggin").serialize());
+  //Skapar ett ajax och sänder.
   $.ajax({
     type: "GET",
     url: "/admin/login",
     data: $("#adminLoggin").serialize(),
-    success: function (response) {
-      console.log(response);
+    success: function (res) {
+      console.log(typeof res);
+      adminToken = res;
+      // console.log(adminToken);
+      document.querySelector(".buttom").disabled = true;
+      $("#loggin_cont").fadeOut(3000);
+      $("#outPut").show();
     },
-    error: function (request, status, error) {
-      alert(request.responseText);
+    error: function (res) {
+      console.log(res);
+    },
+    statusCode: {
+      403: function (response) {
+        // const errorMsg = JSON.parse(response);
+        $("#error-text").text(response.responseText);
+      },
+    },
+  });
+});
+
+/* Går att kalla på funktioner som gör get requests i success på admin loggin*/
+
+$("#loadBokabord").on("click", () => {
+  // console.log(adminToken);
+  $.ajax({
+    type: "GET",
+    url: "/admin/booking",
+    data: { token: adminToken },
+    success: function (res) {
+      $("#loadData").text(res);
+    },
+    error: function (res) {
+      console.log(res);
+    },
+    statusCode: {
+      403: function (res) {
+        // const errorMsg = JSON.parse(response);
+        console.log(res);
+      },
     },
   });
 });
--- routes.js
+++ routes.js
@@ -15,23 +15,22 @@ router.get("/admin", (req, res) => {
 /*
 
 */
+const adminToken = "12312312378126asd1237123hajsdkj81238"; //Behöver autgeneras och ha någon tid rekvid egentligen men nu är den statisk
+
 //post req.body."name" fungerar i post inte i get xD
 router.get("/admin/login", (req, res) => {
   const adminPassword = "Password123"; //adminPassword Kolla med namn + regexp om man känner för det. borde crypteras.
   const adminUser = "Admin";
-  let userAray = { name: adminUser, password: adminPassword };
-  let bokaBord = ["item1", "item2", "osv"];
-  let bokaDator = ["boakdatorobjek1", "boakdatorobjek1", "osv"];
-
-  console.log(req.query.userAdmin);
-  console.log(req.query.password);
+  //unit testing
+  //console.log(req.query.userAdmin);
+  // console.log(req.query.password);
 
   if (req.query.userAdmin == adminUser && req.query.password == adminPassword) {
     //res.send(bokaBord + ' <---Bokabord   Bokadator---> ' + bokaDator);
-    res.send("loginToken: 12312312378126asd1237123hajsdkj81238");
-    console.log("Inlogg succesfull");
-    console.log(bokaBord);
-    console.log(bokaDator);
+    res.send(adminToken);
+    // console.log("Inlogg succesfull");
+    //console.log(bokaBord);
+    //console.log(bokaDator);
   } else {
     console.log(
       "Felaktigt lössenord eller användare: " +
@@ -40,14 +39,15 @@ router.get("/admin/login", (req, res) => {
         "  " +
         req.query.password
     );
-    res.status(403); //Kan kolla status när den försöker läsa får inte komma dit typ error 403 frobbiden acces //Det där funkar :)
+    res.status(403).send("Invalid UserName or Password");
   }
 });
 
-router.get("/booking/", (req, res) => {
+router.get("/admin/booking/", (req, res) => {
+  console.log(req.query);
   if (req.query.token == "12312312378126asd1237123hajsdkj81238") {
     //Kollar behöverighet säker nät
-    res.send("lyckad post"); //Inte svenska bokstäver med res.end  men med res.send kmr köra express resten av kursen
+    res.send("Hämta data så sicka data eller något"); //Inte svenska bokstäver med res.end  men med res.send kmr köra express resten av kursen
   }
 });
 
